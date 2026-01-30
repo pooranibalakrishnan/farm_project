9ec8f8d7a083e26c42362a4b2b97c07da08037fd
pooranibalakrishnan <pooranibalakrishnan2023@gmail.com>
2026-01-30 14:44:19 +0530

    Remove comment for GET all products route

--- server/routes/productRoutes.js
+++ server/routes/productRoutes.js
@@ -2,7 +2,6 @@ const express = require("express");
 const router = express.Router();
 const Product = require("../models/Product");
 
-// GET all products
 router.get("/", async (req, res) => {
   try {
     const products = await Product.find();
