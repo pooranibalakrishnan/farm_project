6444774b5902cf99f3e9ae63eaf60bcb114a5fb0
pooranibalakrishnan <pooranibalakrishnan2023@gmail.com>
2026-01-30 14:43:57 +0530

    Add route to place an order

--- server/routes/orderRoutes.js
+++ server/routes/orderRoutes.js
@@ -2,7 +2,7 @@ const express = require("express");
 const Order = require("../models/Order");
 const router = express.Router();
 
-// ✅ Place Order
+
 router.post("/", async (req, res) => {
   const order = new Order(req.body);
   await order.save();
